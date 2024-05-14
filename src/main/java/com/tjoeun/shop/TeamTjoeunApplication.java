e74f86c1e9d0b70164112f95c1cc1b86a2b70633
tjoeun123 <DavidandLivingstone@gmail.com>
2024-05-14 10:00:22 +0900

    first commit

---
+++ src/main/java/com/tjoeun/shop/TeamTjoeunApplication.java
@@ -0,0 +1,13 @@
+package com.tjoeun.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTjoeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTjoeunApplication.class, args);
+	}
+
+}
